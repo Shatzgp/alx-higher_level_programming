e1f970e074aac0c0c7e3cff51d906a5cc5932e48
Shatzgp <phathumashathini@gmail.com>
2023-06-12 04:31:59 -0700

    updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@ def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
         return(my_list[idx])
     else:
-        return (Non)
+        return (None)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return(0, None)
+    return(len(sentence), sentence[0])
